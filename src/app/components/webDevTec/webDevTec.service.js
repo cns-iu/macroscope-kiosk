8f787681c453c10cd0bf1544929e7dd68ce41e3a
David M. Reagan <dmreagan@iu.edu>
2015-10-05 19:56:02 -0400

    Add P&S content. Format home page to roughly match mockup.

--- src/app/components/webDevTec/webDevTec.service.js
+++ src/app/components/webDevTec/webDevTec.service.js
@@ -9,46 +9,40 @@
   function webDevTec() {
     var data = [
       {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
+        'title': 'Earth',
+        'url': 'http://earth.nullschool.net/',
         'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
+        'logo': 'earth.png'
       },
       {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
+        'title': 'Urban Observatory',
+        'url': 'http://www.urbanobservatory.org/',
         'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
+        'logo': 'urban-observatory.png'
       },
       {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
+        'title': 'IBM Think!',
+        'url': 'https://www.youtube.com/watch?v=8eu6DHBQyGs',
         'description': 'The streaming build system.',
-        'logo': 'gulp.png'
+        'logo': 'ibm-think.png'
       },
       {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
+        'title': 'Mapping Global Society',
+        'url': 'http://www.gdeltproject.org/',
         'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
+        'logo': 'mapping-global-society.png'
       },
       {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
+        'title': 'Academy Scope',
+        'url': 'http://www.nap.edu/academy-scope',
         'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
+        'logo': 'academy-scope.png'
       },
       {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
+        'title': 'A Cultury History',
+        'url': 'https://www.youtube.com/watch?v=4gIhRkCcD4U',
         'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      },
-      {
-        'title': 'Angular Material Design',
-        'url': 'https://material.angularjs.org/#/',
-        'description': 'The Angular reference implementation of the Google\'s Material Design specification.',
-        'logo': 'angular-material.png'
+        'logo': 'cultural-history.png'
       }
     ];
 
