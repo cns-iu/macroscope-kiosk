5dd0734a342baf126b2b55cfd8e5d7aaaf5ac0d4
David M. Reagan <dmreagan@iu.edu>
2015-11-24 13:17:50 -0500

    Add custom dialog for macros. #2.

---
+++ src/app/components/macro/macro.controller.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('macroscopeKiosk')
+    .controller('MacroController', MacroController);
+
+  /** @ngInject */
+  function MacroController($scope, $stateParams, webDevTec, $mdDialog) {
+    var ms = this;
+    ms.macroscope = webDevTec.findById($stateParams.macroId);
+
+
+    $scope.showDialog = function(ev, title, key) {
+      $mdDialog.show({
+        controller: DialogController,
+        template: '<md-dialog><md-dialog-content>' + ms.macroscope[key] + '</md-dialog-content></md-dialog>',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+    };
+    
+    function DialogController($scope, $mdDialog) {
+      $scope.hide = function() {
+        $mdDialog.hide();
+      };
+
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+
+      $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+      };
+    }
+  };
+})();
