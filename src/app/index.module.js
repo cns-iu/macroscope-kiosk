3f738d55306ba5eb77a41fd6071184cad7be0056
David M. Reagan <dmreagan@iu.edu>
2016-06-10 16:11:30 -0400

    Add Google Analytics. Close #46.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,15 +2,17 @@
   'use strict';
 
   angular
-    .module('macroscopeKiosk', ['ngAnimate', 
-                                'ngTouch', 
-                                'ngSanitize', 
-                                'ui.router', 
-                                'ngMaterial', 
+    .module('macroscopeKiosk', ['ngAnimate',
+                                'ngTouch',
+                                'ngSanitize',
+                                'ui.router',
+                                'ngMaterial',
                                 'trustedUrlFilter',
                                 'com.2fdevs.videogular',
                                 'com.2fdevs.videogular.plugins.controls',
                                 'com.2fdevs.videogular.plugins.overlayplay',
-                                'ngIdle']);
+                                'ngIdle',
+                                'angulartics',
+                                'angulartics.google.analytics',]);
 
 })();
