5600689beb29a4769df3605d7bfe8c44fe306e87
axdanbol <daniel_bolin@yahoo.com>
2019-01-24 11:10:59 -0500

    Setup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselItemComponent } from './components/carousel-item/carousel-item.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { IFrameComponent } from './components/iframe/iframe.component';
+import { MacroscopeComponent } from './components/macroscope/macroscope.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ScreenSaverComponent } from './components/screen-saver/screen-saver.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MacroscopeComponent,
+    HeaderComponent,
+    ModalComponent,
+    CarouselComponent,
+    CarouselItemComponent,
+    IFrameComponent,
+    ScreenSaverComponent
   ],
   imports: [
     BrowserModule,
