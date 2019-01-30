01fa1cb9615ac8f56338700c7c1bb674bd3d3817
Shashank Shekhar <shashekh@iu.edu>
2019-01-30 11:05:54 -0500

    fixed linting errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,16 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+/* mocking screensaver component */
+@Component({
+  selector: 'app-screen-saver',
+  template: ''
+})
+class MockScreenSaverComponent {
+  @Input() showScreenSaver: boolean;
+}
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,11 +32,3 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 });
-
-@Component({
-  selector: 'app-screen-saver',
-  template: ''
-})
-class MockScreenSaverComponent {
-  @Input() showScreenSaver: boolean;
-}
