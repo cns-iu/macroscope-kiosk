a6f7648e6caab30c8bee5c21f20bc25dfe973588
axdanbol <daniel_bolin@yahoo.com>
2019-01-31 11:08:36 -0500

    Set active slide to from id in url

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { findIndex } from 'lodash';
 
 import { CarouselItemData } from '../carousel-item/carousel-item.component';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  items: CarouselItemData[] = [];
+export class HomeComponent implements AfterViewInit {
+  items: CarouselItemData[] = [{ iterationId: 11, macroscopeId: 0}, { iterationId: 17, macroscopeId: 5}];
+  @ViewChild(CarouselComponent) carousel: CarouselComponent;
 
-  constructor(private readonly router: Router) { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+  ) { }
+
+  ngAfterViewInit(): void {
+    const { carousel, route: { firstChild: childRoute } } = this;
+    if (childRoute) {
+      const id = +childRoute.snapshot.paramMap.get('iid');
+      const index = findIndex(this.items, ['iterationId', id]);
+      if (index >= 0) { carousel.slideTo(index, 0); }
+    }
+  }
 
   setUrlForActiveSlide(index: number): void {
     const { items, router } = this;
