67837a401b515bcfcf306618e984512e12566a64
Shashank Shekhar <shashekh@iu.edu>
2019-01-31 16:34:11 -0500

    fied sonar issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
@@ -15,6 +15,9 @@ class MockScreenSaverComponent {
 
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let app: any;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -27,88 +30,66 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    app = fixture.debugElement.componentInstance;
+    if (app) {
+      app.isIdle = true;
+    }
+    fixture.detectChanges();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('user activity should set the isIdle to false - click', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     document.dispatchEvent(new Event('click'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - mousemove', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     document.dispatchEvent(new Event('mousemove'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - mousedown', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     document.dispatchEvent(new Event('mousedown'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - keypress', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     document.dispatchEvent(new Event('keypress'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - DOMMouseScroll', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     document.dispatchEvent(new Event('DOMMouseScroll'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - mousewheel', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     document.dispatchEvent(new Event('mousewheel'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - touchmove', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     document.dispatchEvent(new Event('touchmove'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - MSPointerMove', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     document.dispatchEvent(new Event('MSPointerMove'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - window mousemove', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     window.dispatchEvent(new Event('mousemove'));
     expect(app.isIdle).toEqual(false);
   });
 
   it('user activity should set the isIdle to false - window resize', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    app.isIdle = true;
     window.dispatchEvent(new Event('resize'));
     expect(app.isIdle).toEqual(false);
   });
