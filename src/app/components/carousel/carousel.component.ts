2c72f18847d3b9ce120b801a75de9e206d1815a4
axdanbol <daniel_bolin@yahoo.com>
2019-01-31 11:41:19 -0500

    Documented CarouselComponent

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -97,45 +97,86 @@ export class CarouselComponent implements AfterViewInit, OnChanges, OnDestroy {
     this.destroy();
   }
 
+  /**
+   * Transition to the item at a specific index.
+   *
+   * @param index The index of the item to transition to.
+   * @param [speed] The duration of transitioning to the item (ms).
+   */
   slideTo(index: number, speed?: number): void {
     this.swiper.slideToLoop(index, speed);
   }
 
+  /**
+   * Starts autoplay and optionally resets the carousel position to the first item.
+   *
+   * @param [reset=false] Whether to reset the carousel position.
+   */
   startAutoplay(reset = false): void {
     if (reset) { this.slideTo(0, 0); }
     this.swiper.autoplay.start();
   }
 
+  /**
+   * Stops autoplay and optionally resets the carousel position to the first item.
+   *
+   * @param [reset=false] Whether to reset the carousel position.
+   */
   stopAutoplay(reset = false): void {
     if (reset) { this.slideTo(0, 0); }
     this.swiper.autoplay.stop();
   }
 
+  /**
+   * Forwards a value to the `indexChange` output.
+   *
+   * @param index The value to forward.
+   */
   forwardIndexChange(index: number): void {
     this.indexChangeSubject.next(index);
   }
 
+  /**
+   * Creates and attaches to the DOM item components for each slide including duplicates.
+   */
   private initialize(): void {
     const roots = this.selectItemComponentRoots();
     this.components = map(roots, r => this.createItemComponentForRoot(r));
   }
 
+  /**
+   * Detaches and destroys all item components.
+   */
   private destroy(): void {
     forEach(this.components, c => this.destroyItemComponent(c));
     this.components = undefined;
   }
 
-  // tslint:disable-next-line:member-ordering
-  private readonly reinitialize = debounce(function reinitializeImpl(this: CarouselComponent): void {
+
+  /**
+   * Destroys all old item components and creates new ones with the current data.
+   */
+  private readonly reinitialize = debounce(function reinitializeImpl(this: CarouselComponent): void { // tslint:disable-line:member-ordering
     this.destroy();
     this.initialize();
     this.changeDetector.markForCheck();
   });
 
+  /**
+   * Selects all slide elements on which item components should be attached.
+   *
+   * @returns A `NodeList` with the selected elements.
+   */
   private selectItemComponentRoots(): NodeListOf<HTMLElement> {
     return (this.element.nativeElement as HTMLElement).querySelectorAll('.swiper-slide');
   }
 
+  /**
+   * Creates a `CarouselItemComponent`.
+   *
+   * @param data The data to provide as input to the component.
+   * @returns A reference to the component.
+   */
   private createItemComponent(data: any): ComponentRef<CarouselItemComponent> {
     const { appRef, injector, itemFactory } = this;
     const component = itemFactory.create(injector);
@@ -145,6 +186,12 @@ export class CarouselComponent implements AfterViewInit, OnChanges, OnDestroy {
     return component;
   }
 
+  /**
+   * Creates a `CarouselItemComponent` and attaches it to the DOM.
+   *
+   * @param root The `HTMLElement` to attach the component to.
+   * @returns A reference to the component.
+   */
   private createItemComponentForRoot(root: HTMLElement): ComponentRef<CarouselItemComponent> {
     // Swiper places the real (adjusted for duplicates) slide index on this attribute
     const index = +root.dataset.swiperSlideIndex;
@@ -155,11 +202,22 @@ export class CarouselComponent implements AfterViewInit, OnChanges, OnDestroy {
     return component;
   }
 
+  /**
+   * Destroys a `CarouselItemComponent` including detaching it from the DOM.
+   *
+   * @param component The component to destroy.
+   */
   private destroyItemComponent(component: ComponentRef<CarouselItemComponent>): void {
     this.appRef.detachView(component.hostView);
     component.destroy();
   }
 
+  /**
+   * Updates the data for a `CarouselItemComponent`.
+   *
+   * @param component The component for which to update its data.
+   * @param data The new data for the item component.
+   */
   private updateItemComponent(component: ComponentRef<CarouselItemComponent>, data: any): void {
     const { changeDetectorRef, instance } = component;
 
