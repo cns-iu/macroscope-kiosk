9e7295d940c61fe1e66812b27219737fb36dd47d
Gurjaspal <gurjaspal.bedi@gmail.com>
2019-02-06 13:13:33 -0500

    New URL and reuse of data object in data service(WIP)

--- src/app/components/iframe/iframe.component.ts
+++ src/app/components/iframe/iframe.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { MacroscopeDataService } from '../../shared/services/macroscope-data/macroscope-data.service';
 
 @Component({
   selector: 'app-iframe',
   templateUrl: './iframe.component.html',
   styleUrls: ['./iframe.component.scss']
 })
-export class IFrameComponent implements OnInit {
+export class IFrameComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  activatedRouteParamsSubscription: Subscription;
+  activeMacroscopeData: any;
+  macroscopeUrl: SafeResourceUrl;
+
+  constructor(
+    private readonly macroscopeDataService: MacroscopeDataService,
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly sanitize: DomSanitizer
+    ) {
+    this.macroscopeDataService.data.subscribe((data: any) => {
+      this.updateMacroscope(data);
+    });
+  }
+
+  updateMacroscope(data: any) {
+
+    this.activatedRouteParamsSubscription = this.activatedRoute.params.subscribe((params) => {
+      const iterationId = params['iid'];
+      const macroId = params['mid'];
+      const activeMacroscopeData = data.filter((row: any) => {
+        return row['iterationId'] === parseInt(iterationId, 10) && row['macroId'] === parseInt(macroId, 10);
+      });
+      if (activeMacroscopeData && activeMacroscopeData.length > 0) {
+      this.macroscopeUrl = this.sanitize.bypassSecurityTrustResourceUrl(activeMacroscopeData[0]['url']);
+      }
+    });
+  }
 
   ngOnInit() {
   }
+
+  ngOnDestroy() {
+    this.activatedRouteParamsSubscription.unsubscribe();
+  }
+
 
 }
