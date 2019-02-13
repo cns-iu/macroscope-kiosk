079eca58b7003e5c5eb3cf249290814b9b6e39ef
Gurjaspal <gurjaspal.bedi@gmail.com>
2019-02-13 12:51:49 -0500

    Code comments for the pipe.

--- src/app/shared/safe-pipe/safe.pipe.ts
+++ src/app/shared/safe-pipe/safe.pipe.ts
@@ -8,8 +8,20 @@ import { DomSanitizer, SafeHtml, SafeResourceUrl, SafeScript, SafeStyle, SafeUrl
   name: 'safe'
 })
 export class SafePipe implements PipeTransform {
+
+  /**
+   * Creates an instance of safe pipe.
+   * @param sanitizer Angular's DomSanitizer
+   */
   constructor(protected sanitizer: DomSanitizer) { }
 
+
+  /**
+   * Sanitized the given value for binding
+   * @param value The value that needs to be trusted
+   * @param type Sanitization type, this could be html,style,script,url and resourceUrl
+   * @returns  Returns the sanitized valye for given sanitization type
+   */
   public transform(value: any, type: string): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
     switch (type) {
       case 'html': return this.sanitizer.bypassSecurityTrustHtml(value);
