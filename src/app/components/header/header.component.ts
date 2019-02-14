cbd5fb6cdcb7e7976595bc3afec1e92d2da066d8
Gurjaspal <gurjaspal.bedi@gmail.com>
2019-02-14 14:23:17 -0500

    code comments

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+
+/**
+ * Header component holds the left content, logo of Application and right content.
+ * Left Content: Could be back button or Modal click
+ * Right Content: Different modal clicks based on users location
+ */
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
